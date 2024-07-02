a3563eebc769c5d8ae7b8fb2115904950f2e0e97
Aidan Cooper <aidancooper2003@gmail.com>
2024-07-02 11:54:13 -0400

    Fixed regression where end game button no longer worked

--- scripts/ui/ui_manager.gd
+++ scripts/ui/ui_manager.gd
@@ -27,6 +27,7 @@ func _on_area_2d_area_entered(area):
 	finalTimerText.text = "FINAL TIME: " + str(snapped(time, 0.001))
 
 
+
 func _on_player_health_changed(newHealth: int):
 	healthText.text = "Player Health: " + str(newHealth)
 	if newHealth == 0:
