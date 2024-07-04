eafa0e73afa59335a8eebcc06fed30a7c749e184
aidcoope <aidcoope@iu.edu>
2024-07-04 09:16:22 -0400

    Refactored damage components

--- scripts/entity-components/continuous_damage_dealer.gd
+++ scripts/entity-components/continuous_damage_dealer.gd
@@ -1,11 +1,13 @@
+class_name ContinuousDamageDealer
+
 extends Area2D
 
-class_name ContinuousDamageDealer
+
 
 signal damage_dealt
 
-@export var damage: float
-@export var damageDelay: float
+@export var _damage: float
+@export var _damage_delay: float
 
 var _is_active: bool = true
 var _damage_disabled: bool
@@ -13,15 +15,15 @@ var _targets: Dictionary = {}
 
 func _physics_process(_delta):
 	for area in _targets:
-		if _targets.get(area) < Time.get_ticks_msec() - damageDelay * 1000:
-			area.damage(damage)
+		if _targets.get(area) < Time.get_ticks_msec() - _damage_delay * 1000:
+			area.damage(_damage)
 
 
 func _on_area_entered(area):
 	if _is_active:
 		if area is HitboxComponent:
 			_targets[area] = Time.get_ticks_msec()
-			area.damage(damage)
+			area.damage(_damage)
 
 
 func _on_area_exited(area):
--- scripts/entity-components/damage_dealer_component.gd
+++ scripts/entity-components/damage_dealer_component.gd
@@ -1,15 +1,15 @@
-class_name DamageDealerComponent
-
 extends Area2D
+
+class_name DamageDealerComponent
 
 signal damage_dealt
 
-@export var damage: float
+@export var _damage: float
 
 var _is_active: bool = true
 
 func _on_area_entered(area):
 	if _is_active:
 		if area is HitboxComponent:
-			area.damage(damage) 
+			area.damage(_damage) 
 			damage_dealt.emit()
