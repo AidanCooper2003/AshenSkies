1826a10bd55d215f9a9e10d865c4dd7f598bde79
aidcoope <aidcoope@iu.edu>
2024-07-05 11:37:29 -0400

    Finished refactoring ui manager

--- scripts/ui/ui_manager.gd
+++ scripts/ui/ui_manager.gd
@@ -13,41 +13,38 @@ extends Node2D
 var _last_selected_weapon := 0
 var _time: float = 0.0
 
-
-
 func _process(delta):
 	if _timer_text != null:
 		_time += delta
 		_timer_text.text = "Time: " + str(snapped(_time, 0.001))
 
+
 func _on_area_2d_area_entered(area):
 	_final_timer_text.text = "FINAL TIME: " + str(snapped(_time, 0.001))
 
 
-
-func _on_player_health_changed(newHealth: int):
-	_health_text.text = "Player Health: " + str(newHealth)
-	if newHealth == 0:
+func _on_player_health_changed(new_health: int):
+	_health_text.text = "Player Health: " + str(new_health)
+	if new_health == 0:
 		_death_text.visible = true
 
 
-func _on_player_new_weapon_selected(newWeapon: int):
+func _on_player_new_weapon_selected(new_weapon: int):
 	_weapon_slots[_last_selected_weapon].modulate = _deselected_slot_color
-	_weapon_slots[newWeapon].modulate = _selected_slot_color
-	_last_selected_weapon = newWeapon
+	_weapon_slots[new_weapon].modulate = _selected_slot_color
+	_last_selected_weapon = new_weapon
 
 
-func _on_player_durability_changed(_current_weapon: int, durability: int):
-	_weapon_slots[_current_weapon].get_child(2).value = durability
+func _on_player_durability_changed(current_weapon: int, durability: int):
+	_weapon_slots[current_weapon].get_child(2).value = durability
 
-func _on_player_weapon_slot_changed(weaponSlot: int, weaponName: String):
-	if weaponName == "":
-		_weapon_slots[weaponSlot].get_child(1).texture = null
-		_weapon_slots[weaponSlot].get_child(2).visible = false
+
+func _on_player_weapon_slot_changed(weapon_slot: int, weapon_name: String):
+	if weapon_name == "":
+		_weapon_slots[weapon_slot].get_child(1).texture = null
+		_weapon_slots[weapon_slot].get_child(2).visible = false
 		return
-	_weapon_slots[weaponSlot].get_child(1).texture = (
-			load("res://sprites/item_icons/" + CSVManager.get_item_icon(weaponName))
+	_weapon_slots[weapon_slot].get_child(1).texture = (
+			load("res://sprites/item_icons/" + CSVManager.get_item_icon(weapon_name))
 	)
-	_weapon_slots[weaponSlot].get_child(2).visible = true
-
-	
+	_weapon_slots[weapon_slot].get_child(2).visible = true
