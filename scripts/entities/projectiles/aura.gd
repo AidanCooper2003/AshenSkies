d46d73d7ef4cb84a77b8322ee630c975c11407ea
aidcoope <aidcoope@iu.edu>
2024-08-28 12:08:05 -0400

    Added animation for candleman aura

--- scripts/entities/projectiles/aura.gd
+++ scripts/entities/projectiles/aura.gd
@@ -4,6 +4,10 @@ extends Node2D
 
 var connected_condition: String
 
+func _ready():
+	if $AnimationPlayer:
+		$AnimationPlayer.play("start_aura")
+
 func end_aura(condition):
 	if condition == connected_condition:
 		queue_free()
